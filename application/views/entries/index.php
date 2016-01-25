44161f9e2d275edf15e190146f6b42ae1df18330
ashleyyy <ashleyfisher@gmail.com>
2016-01-24 18:46:16 -0800

    added timestamps

--- application/views/entries/index.php
+++ application/views/entries/index.php
@@ -1,21 +1,18 @@
-
-
 <?php foreach ($entries as $entry): ?>
 
-        <h3><a href="<?php echo site_url('entries/'.$entry['id']); ?>"><?php echo $entry['user']; ?></a></h3>
-        <div class="main">
-          <?php echo $entry['comments']; ?>
-        </div>
+  <h3><?php echo $entry['user']; ?></h3>
+  <?php echo $entry['t']; ?>
+  <div class="comments">
+    <?php echo $entry['comments']; ?>
+  </div>
 
-        <?php 
+  <a href="<?php echo site_url('entries/'.$entry['id']); ?>">View on single page</a>
+
+<?php 
   $id = $entry['id'];
   echo form_open('entries/delete/'.$id); 
 ?>
 
-<input type="submit" name="delete" value="Delete Entry" />
-
-
-</form>
-
+<input type="submit" name="delete" value="Delete Entry" /></form>
 
 <?php endforeach; ?>
